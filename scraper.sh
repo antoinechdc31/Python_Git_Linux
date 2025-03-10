dab7fd7e22c48a38ecff6fb9ed10336fa2dd3055
Alix Lanotte-Moro <152471065+alixlm@users.noreply.github.com>
2025-03-10 14:35:26 +0100

    Delete scraper.sh

--- scraper.sh
+++
@@ -1,9 +0,0 @@
-
-URL="https://www.x-rates.com/calculator/?from=USD&to=EUR&amount=1"
-
-HTML=$(curl -s "$URL")
-RATE=$(echo "$HTML" | grep 'ccOutputRslt' | sed -E 's/.*ccOutputRslt">([0-9.]+).*/\1/')
-
-DATE=$(date '+%Y-%m-%d %H:%M:%S')
-echo "$DATE,$RATE" >> rate_log.csv
-echo "[$DATE] Taux USD/EUR : $RATE"
